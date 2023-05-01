e4446ca64d515e67e5ed4e5524e6da799b1a0eb4
heedii <127703762+heedii@users.noreply.github.com>
2023-05-01 00:34:12 -0700

    Fullbright dont do nothing

---
+++ src/main/java/blebdapleb/arsenic/arsenic/module/mods/render/Fullbright.java
@@ -0,0 +1,16 @@
+package blebdapleb.arsenic.arsenic.module.mods.render;
+
+import blebdapleb.arsenic.arsenic.eventbus.ArsenicSubscribe;
+import blebdapleb.arsenic.arsenic.module.Module;
+import blebdapleb.arsenic.arsenic.module.ModuleCategory;
+
+public class Fullbright extends Module {
+    public Fullbright() {
+        super("FullBright", KEY_UNBOUND, ModuleCategory.RENDER, "Night Vision");
+    }
+
+    @ArsenicSubscribe
+    public void onEnable() {
+        
+    }
+}
