2eb4926af8f62f1bfe4fecb24037d63fd55a3f51
SiyuuAcc <132279124+SiyuuAcc@users.noreply.github.com>
2023-05-02 02:46:26 +0900

    Update Fullbright.java

--- src/main/java/blebdapleb/arsenic/arsenic/module/mods/render/Fullbright.java
+++ src/main/java/blebdapleb/arsenic/arsenic/module/mods/render/Fullbright.java
@@ -4,18 +4,23 @@ import blebdapleb.arsenic.arsenic.event.events.EventTick;
 import blebdapleb.arsenic.arsenic.eventbus.ArsenicSubscribe;
 import blebdapleb.arsenic.arsenic.module.Module;
 import blebdapleb.arsenic.arsenic.module.ModuleCategory;
+import blebdapleb.arsenic.arsenic.module.setting.settings.SettingMode;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 
 public class Fullbright extends Module {
 
     public Fullbright() {
-        super("FullBright", KEY_UNBOUND, ModuleCategory.RENDER, "it gives you NightVision Effect. lol");
+        super("FullBright", KEY_UNBOUND, ModuleCategory.RENDER, "Fullbright Module.",
+                new SettingMode("Mode", "Potion", "Potion")
+        );
     }
 
     @ArsenicSubscribe
     public void onTick(EventTick eventTick) {
-        mc.player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 5200, 1));
+        if (getSetting(0).asMode().isMode("Potion")) {
+            mc.player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 5200, 1));
+        }
     }
 
     @Override
