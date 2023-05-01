dfcb2103d82625fad35d6e14ee2d66e5cc0ff702
SiyuuAcc <132279124+SiyuuAcc@users.noreply.github.com>
2023-05-02 00:34:34 +0900

    Update Fullbright.java

--- src/main/java/blebdapleb/arsenic/arsenic/module/mods/render/Fullbright.java
+++ src/main/java/blebdapleb/arsenic/arsenic/module/mods/render/Fullbright.java
@@ -4,15 +4,23 @@ import blebdapleb.arsenic.arsenic.event.events.EventTick;
 import blebdapleb.arsenic.arsenic.eventbus.ArsenicSubscribe;
 import blebdapleb.arsenic.arsenic.module.Module;
 import blebdapleb.arsenic.arsenic.module.ModuleCategory;
-import blebdapleb.arsenic.arsenic.util.ISimpleOption;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 
 public class Fullbright extends Module {
+
     public Fullbright() {
-        super("FullBright", KEY_UNBOUND, ModuleCategory.RENDER, "Night Vision");
+        super("FullBright", KEY_UNBOUND, ModuleCategory.RENDER, "it gives you NightVision Effect. lol");
     }
 
     @ArsenicSubscribe
     public void onTick(EventTick eventTick) {
-        ((ISimpleOption<Double>)(Object)mc.options.getGamma()).setValueUnrestricted(100.0d);
+        mc.player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 5200, 1));
+    }
+
+    @Override
+    public void onDisable(boolean inWorld) {
+        mc.player.removeStatusEffect(StatusEffects.NIGHT_VISION);
+        super.onDisable(inWorld);
     }
 }
