5516dc0811529ff401c67486b18c478e858fb976
heedii <127703762+heedii@users.noreply.github.com>
2023-05-01 05:49:29 -0700

    Jetpack mode in flight

--- src/main/java/blebdapleb/arsenic/arsenic/module/mods/movement/Flight.java
+++ src/main/java/blebdapleb/arsenic/arsenic/module/mods/movement/Flight.java
@@ -4,12 +4,14 @@ import blebdapleb.arsenic.arsenic.event.events.EventTick;
 import blebdapleb.arsenic.arsenic.eventbus.ArsenicSubscribe;
 import blebdapleb.arsenic.arsenic.module.Module;
 import blebdapleb.arsenic.arsenic.module.ModuleCategory;
+import blebdapleb.arsenic.arsenic.module.setting.Setting;
 import blebdapleb.arsenic.arsenic.module.setting.settings.SettingMode;
 
 public class Flight extends Module {
     public Flight() {
-        super("Flight", KEY_UNBOUND, ModuleCategory.MOVEMENT, "Allows you to fly.");
-        new SettingMode("Mode", "Vanilla", "Vanilla", "Jetpack");
+        super("Flight", KEY_UNBOUND, ModuleCategory.MOVEMENT, "Allows you to fly.",
+        new SettingMode("Mode", "Vanilla", "Vanilla", "Jetpack")
+        );
     }
 
     @ArsenicSubscribe
