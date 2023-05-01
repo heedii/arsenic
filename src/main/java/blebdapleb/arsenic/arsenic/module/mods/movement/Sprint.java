39f28875a1c49b5fb56d81763dadaace0a5fbc49
heedii <127703762+heedii@users.noreply.github.com>
2023-04-30 19:18:04 -0700

    Create Sprint.java

---
+++ src/main/java/blebdapleb/arsenic/arsenic/module/mods/movement/Sprint.java
@@ -0,0 +1,14 @@
+package blebdapleb.arsenic.arsenic.module.mods.movement;
+
+import blebdapleb.arsenic.arsenic.module.Module;
+import blebdapleb.arsenic.arsenic.module.ModuleCategory;
+
+public class Sprint extends Module {
+
+    public Sprint() {
+        super("Sprint", KEY_UNBOUND, ModuleCategory.MOVEMENT, "Allows you to sprint.");
+    }
+
+    @Override
+    public void onTick()
+}
